8e4da4334761a799b99b914390acb22070cc40f0
Tianshu Wang <wwwwts@me.com>
2021-01-16 11:25:15 -0500

    Add files via upload

---
+++ eval_anon1_new.py
@@ -0,0 +1,39 @@
+import keras
+import sys
+import h5py
+import numpy as np
+import keras
+import keras.backend as K
+from keras import initializers
+import tensorflow_model_optimization as tfmot
+import tensorflow as tf
+
+test_data_filename = "./data/clean_test_data.h5"
+bad_net = "./anonymous_1_bd_net.h5"
+new_net = "./models/anon1_new.h5"
+img_filename = sys.argv[1]
+
+def data_preprocess(x_data):
+    return x_data/255
+
+def main():
+	img = Image.open(img_filename)
+    x = np.array(img)
+    x = x.reshape(1, 55, 47, 3)
+    x = data_preprocess(x)
+
+	with tfmot.sparsity.keras.prune_scope():
+		bd_model = keras.models.load_model(bad_net)
+		new_model = keras.models.load_model(new_net)
+
+	pred1 = np.argmax(bd_model.predict(x), axis=1)
+	pred2 = np.argmax(new_model.predict(x), axis=1)
+
+	if pred1 == pred2:
+		print(pred1[0])
+	else:
+		print(1283)
+
+
+if __name__ == '__main__':
+    main()
