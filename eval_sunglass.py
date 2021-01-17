aa84f25f5898a5ddcad69b5b18344f4cf405410f
Tianshu Wang <wwwwts@me.com>
2021-01-16 23:19:24 -0500

    Update eval_sunglass.py

--- eval_sunglass.py
+++ eval_sunglass.py
@@ -7,6 +7,8 @@ import keras.backend as K
 from keras import initializers
 import tensorflow_model_optimization as tfmot
 import tensorflow as tf
+from PIL import Image
+
 
 bad_net =  "./sunglasses_bd_net.h5"
 new_net = "./models/sunglasses_new.h5"
